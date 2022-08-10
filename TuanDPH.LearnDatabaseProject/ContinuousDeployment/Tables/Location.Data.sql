a76b13bcb716e5f9d0ee5d885e7f40a441fb5760
tuandph <tuantanhero@gmail.com>
2022-08-10 17:09:35 +0700

    Init Database Project
    Init Database Project

---
+++ TuanDPH.LearnDatabaseProject/ContinuousDeployment/Tables/Location.Data.sql
@@ -0,0 +1,31 @@
+﻿TRUNCATE TABLE [dbo].[Location]
+GO
+
+INSERT INTO [dbo].[Location]
+           ([Id]
+           ,[Name])
+     VALUES
+           (1
+           ,'Viet Nam')
+GO
+INSERT INTO [dbo].[Location]
+           ([Id]
+           ,[Name])
+     VALUES
+           (2
+           ,'Malay')
+GO
+INSERT INTO [dbo].[Location]
+           ([Id]
+           ,[Name])
+     VALUES
+           (3
+           ,'Canada')
+GO
+INSERT INTO [dbo].[Location]
+           ([Id]
+           ,[Name])
+     VALUES
+           (4
+           ,'Tobe Defined')
+GO
